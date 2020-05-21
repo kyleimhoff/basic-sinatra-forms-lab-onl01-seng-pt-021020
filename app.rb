5f630fb0a1b03507dc961a9883469ebbe4ee3e5f
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-21 01:09:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 get '/team' do 
-  @team_name = params[:team_name]
+  @name = params[:name]
   @coach = params[:coach]
   @pg = params[:pg]
   @sg = params[:sg]
